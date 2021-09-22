b5dfbf1df8c301af48b3fa35cecc981a4158909d
Charlie Weber <charlestweber@gmail.com>
2021-09-22 13:53:01 -0700

    good fail on findprime test
    Co-Authored-By: Paige Tiedeman <paigetiedeman@gmail.com>

--- PrimeSifting/Models/Prime.cs
+++ PrimeSifting/Models/Prime.cs
@@ -5,10 +5,18 @@ namespace PrimeSifting
 {
   public class Prime
   {
-    public void findPrime(int num)
+    public List<int> findNumbers(int num)
     {
-      List<int> numberList = new List();
-      for ()
+      List<int> numList = new List<int> {};
+      for (int i=2; i<= num ; i++)
+      {
+        numList.Add(i);
+      }
+      return numList;
+    }
+    public List<int> findPrime(List<int> numList)
+    {
+      return numList;
     }
   }
 }
